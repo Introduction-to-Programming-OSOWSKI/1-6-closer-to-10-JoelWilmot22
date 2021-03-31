7552de2d62ad6f1af05cb88296f463430a3f1d8a
Wilmot Joel <818032@stma.k12.mn.us>
2021-03-31 10:13:22 -0500

    Try 1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#WRITE YOUR CODE IN THIS FILE
+def close10(x,y):
+    if abs(x-10)>abs(y-10):
+        return y
+
+    elif abs(x-10)<abs(y-10):
+        return x
+
+    else:
+        return 0
+
+
